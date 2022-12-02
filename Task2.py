2a450ae12880650bc0f820cb23998947aa728d40
Olesia Barannik <112662408+OlesiaBarannik@users.noreply.github.com>
2022-12-02 20:54:39 +0200

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,35 @@
+def merge(a,b):
+   c=[]
+   i=j=0
+   while i<len(a) and j<len(b):
+         if a[i]<b[j]:
+           c.append (a[i])
+           i+=1
+         else:
+           c.append(b[j])
+           j+=1
+   while i < len(a):
+       c.append(a[i])
+       i += 1
+   while j < len(b ):
+       c.append(b[j])
+       j += 1
+   return c
+
+def splt(list):
+    if len(list)==1:
+        return list
+    mdl = len(list)//2
+    rg = []
+    lf = []
+    for i in range(len(list)):
+        if i < mdl:
+            lf.append(list[i])
+        else:
+            rg.append(list[i])
+    rg = splt(rg)
+    lf = splt(lf)
+    return merge(lf, rg)
+
+print(splt([5,2,9,6,0,3,4,1,2]))
+
